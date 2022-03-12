d7ab0ca52ed4c0f8d81e37bed165b8efc1597328
Victor-Higa1997 <victor_nagahara@estudante.sesisenai.org.br>
2022-03-11 21:34:05 -0300

    Exercícios finalizados

---
+++ Administration/Administration.Core/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿namespace Administration.Core
+{
+    public static class Calculadora 
+    {
+        public static int somar(int valor1, int valor2)
+        {
+            return valor1 + valor2; 
+        }
+
+        public static int somar(int valor1, int valor2, int valor3)
+        {
+            return valor1 + valor2 + valor3;
+        }
+        public static int subtrair(int valor1, int valor2)
+        {
+            return valor1 - valor2; 
+        }
+        public static int subtrair(int valor1, int valor2, int valor3)
+        {
+            return valor1 - valor2 - valor3;
+        }
+        public static int multiplicar(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+        public static int multiplicar(int valor1, int valor2, int valor3)
+        {
+            return valor1 * valor2 * valor3;
+        }
+        public static int dividir(int valor1, int valor2)
+        {
+            return (valor1 / valor2);
+        }
+    }
+}
---
+++ Administration/AdministrationConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Administration.Core;
+
+int somar = Calculadora.somar(5, 13);
+int subtrair = Calculadora.subtrair(5, 13);
+int multiplicar = Calculadora.multiplicar(5, 13);
+int dividir = Calculadora.dividir(5, 13);
+
+int somar2 = Calculadora.somar(5, 13, 4);
+int subtrair2 = Calculadora.subtrair(5, 13, 5);
+int multiplicar2 = Calculadora.multiplicar(5, 13, 2);
+
+Console.WriteLine("------------- RESULTADO DAS OPERAÇÕES -------------");
+Console.WriteLine($"A soma dos valores é: {somar2}");
+Console.WriteLine($"A subtração dos valores é: {subtrair2}");
+Console.WriteLine($"A multiplicação dos valores é: {multiplicar2}");
+Console.WriteLine($"A divisão dos valores é: {dividir}");
